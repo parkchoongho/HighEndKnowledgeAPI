755f5f56a5021b213178ef5ff4dcad5125e28364
parkchoongho <34790763+parkchoongho@users.noreply.github.com>
2019-11-13 20:07:46 +0900

    add models

---
+++ models/quiz.js
@@ -0,0 +1,29 @@
+const mongoose = require("mongoose");
+const Joi = require("@hapi/joi");
+
+const { Schema, model } = mongoose;
+
+const quizSchema = new Schema({
+  title: { type: String, required: true },
+  contents: { type: String, required: true },
+  answer: { type: String, required: true },
+  tags: [{ type: String }],
+  lat: { type: Number, required: true },
+  lon: { type: Number, required: true }
+});
+
+const Quiz = model("Quiz", quizSchema);
+
+function validateQuiz(quiz) {
+  const schema = Joi.object({
+    title: Joi.string(),
+    contents: Joi.string(),
+    answer: Joi.string(),
+    tags: Joi.array().items(Joi.string()),
+    lat: Joi.number(),
+    lon: Joi.number()
+  });
+  return schema.validate(quiz);
+}
+
+module.exports = { User, validateQuiz };
---
+++ models/user.js
@@ -0,0 +1,47 @@
+const mongoose = require("mongoose");
+const Joi = require("@hapi/joi");
+
+const { Schema, model } = mongoose;
+
+const userSchema = new Schema({
+  name: {
+    type: String,
+    required: true,
+    minlength: 3,
+    maxlength: 30,
+    lowercase: true
+  },
+  password: {
+    type: String,
+    required: true,
+    minlength: 3,
+    maxlength: 30,
+    lowercase: true
+  },
+  birth: Date,
+  gender: String,
+  complete_quizs: [{ type: mongoose.Types.ObjectId, ref: "Quiz" }]
+});
+
+const User = model("User", userSchema);
+
+function validateUser(user) {
+  const schema = Joi.object({
+    name: Joi.string()
+      .alphanum()
+      .lowercase()
+      .min(3)
+      .max(30),
+    password: Joi.string()
+      .alphanum()
+      .lowercase()
+      .min(3)
+      .max(30),
+    birth: Joi.date(),
+    gender: Joi.string(),
+    complete_quizs: Joi.array().items(Joi.string())
+  });
+  return schema.validate(user);
+}
+
+module.exports = { User, validateUser };

7c7d9c2d6ec4507aca5452cb433496b982d68f18
parkchoongho <34790763+parkchoongho@users.noreply.github.com>
2019-11-18 15:07:08 +0900

    add admin

--- routers/quiz.js
+++ routers/quiz.js
@@ -6,6 +6,12 @@ const { Quiz, validateQuiz } = require("../models/quiz");
 const router = express.Router();
 
 router.post("/", auth.authenticate(), async (req, res, next) => {
+  console.log(req.user);
+  if (!req.user.admin) {
+    res.status(400).json({ result: false, error: "권한이 없습니다" });
+    next();
+    return;
+  }
   const { title, contents, answer, tags, lat, lon } = req.body;
   if (validateQuiz(req.body).error) {
     res.status(400).json({ result: false, error: "양식에 맞지않음" });

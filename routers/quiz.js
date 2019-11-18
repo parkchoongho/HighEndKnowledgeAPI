db582dcadcc6727e92acfcadf57ddaca87569d17
parkchoongho <34790763+parkchoongho@users.noreply.github.com>
2019-11-18 14:51:58 +0900

    complete feature/quiz

--- routers/quiz.js
+++ routers/quiz.js
@@ -1,5 +1,28 @@
 const express = require("express");
 
+const auth = require("../common/auth")();
+const { Quiz, validateQuiz } = require("../models/quiz");
+
 const router = express.Router();
+
+router.post("/", auth.authenticate(), async (req, res, next) => {
+  const { title, contents, answer, tags, lat, lon } = req.body;
+  if (validateQuiz(req.body).error) {
+    res.status(400).json({ result: false, error: "양식에 맞지않음" });
+    next();
+    return;
+  }
+  const quiz = new Quiz({
+    title,
+    contents,
+    answer,
+    tags,
+    lat,
+    lon
+  });
+  await quiz.save();
+  res.json({ result: true });
+  next();
+});
 
 module.exports = router;

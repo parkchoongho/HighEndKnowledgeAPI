07d224f767f062f0b6e940ce0310a50932c27aaa
parkchoongho <34790763+parkchoongho@users.noreply.github.com>
2019-11-19 16:57:21 +0900

    add for delpoy

--- index.js
+++ index.js
@@ -10,9 +10,10 @@ const config = require("./common/jwt_config");
 const auth = require("./common/auth")();
 
 const PORT = process.env.PORT || 3000;
-const dbURI = process.env.MONGODB_URI || "mongodb://localhost/highendknowledge";
-// "mongodb://heroku_z2bk21tq:p9ca32e92mprk5gtajspae0pna@ds033469.mlab.com:33469/heroku_z2bk21tq";
-
+const dbURI =
+  process.env.MONGODB_URI ||
+  "mongodb://heroku_z2bk21tq:p9ca32e92mprk5gtajspae0pna@ds033469.mlab.com:33469/heroku_z2bk21tq";
+// "mongodb://localhost/highendknowledge";
 const app = express();
 
 app.use(helmet());
